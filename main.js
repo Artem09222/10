138590a2259acfcd1f1c053aedbe4c34d9716daa
Artem09222 <107929764+Artem09222@users.noreply.github.com>
2023-04-11 16:34:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,6 @@
 //     getTitle.style.color = 'red';
 // }
 
-// getButton.addEventListener('click', printInfo)
-
 let getButton = document.querySelector(".but");
 let getTitle = document.querySelector(".output");
 let getInput1 = document.querySelector(".text-input1");
@@ -22,3 +20,19 @@ function printInfo() {
 }
 
 getButton.addEventListener("click", printInfo);
+
+let getBut = document.querySelector(".button");
+let getText = document.querySelector(".out");
+let getInputs = document.querySelector(".text-inputs");
+let getInput = document.querySelector(".text-input");
+
+function printTitle() {
+  let a = getInputs.value;
+  let b = getInput.value;
+  getInputs = Number(a);
+  getInput = Number(b);
+  let square = getInputs * getInput;
+  getText.textContent = square;
+}
+
+getBut.addEventListener("click", printTitle);
